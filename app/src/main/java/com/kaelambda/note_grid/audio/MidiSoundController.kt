d2e3b72d065c28e73ff550de064e1090516b978a
adam <kaelambda@gmail.com>
2023-10-23 21:37:10 -0400

    Implemented an instrument selector.

--- app/src/main/java/com/kaelambda/note_grid/audio/MidiSoundController.kt
+++ app/src/main/java/com/kaelambda/note_grid/audio/MidiSoundController.kt
@@ -4,9 +4,9 @@ import cn.sherlock.com.sun.media.sound.SoftSynthesizer
 import jp.kshoji.javax.sound.midi.ShortMessage
 import javax.inject.Inject
 
-class MidiSoundController @Inject constructor() {
-    @Inject
-    lateinit var synth: SoftSynthesizer
+class MidiSoundController @Inject constructor(
+    private val synth: SoftSynthesizer
+) {
     fun play(scaleDegree: Int) {
         val msg = ShortMessage()
         msg.setMessage(ShortMessage.NOTE_ON, 0, getNote(scaleDegree), 127)
@@ -17,6 +17,22 @@ class MidiSoundController @Inject constructor() {
         val msg = ShortMessage()
         msg.setMessage(ShortMessage.NOTE_OFF, 0, getNote(scaleDegree), 127)
         synth.receiver.send(msg, -1)
+    }
+
+    fun getCurrentInstrument(): String {
+        return synth.loadedInstruments[synth.channels[0].program].name
+    }
+
+    fun getAvailableInstruments(): List<String> {
+        return synth.loadedInstruments.map { instrument -> instrument.name }
+    }
+
+    fun selectInstrument(name: String) {
+        synth.channels[0].programChange(
+            synth.loadedInstruments.find {
+                    instrument -> instrument.name == name
+            }?.patch?.program ?: 0
+        )
     }
 
     private fun getNote(scaleDegree: Int): Int {
